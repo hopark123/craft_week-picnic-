ea218c5c2740fab542e14ce370d60976b09abd51
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2022-04-26 12:49:21 +0900

    background scroll

--- Assets/Scripts/GameScene/UI/BgScroll.cs
+++ Assets/Scripts/GameScene/UI/BgScroll.cs
@@ -5,48 +5,24 @@ using UnityEngine;
 
 public class BgScroll : MonoBehaviour
 {
-    public RectTransform canvas;
-    public Transform goal;
-    public Transform playerTrans;
-    public Transform respawnPoint;
-
-    Player player;
-    float playerSpeed;
-
-    float canvasSize;
-    float bgSize;
-
-    float diff;
-    float mapLength;
-
-    SpriteRenderer sr;
+    Material mat;
+    Vector2 offset;
     
+    [SerializeField]
+    private float velocity;
+
     void Awake()
     {
-        sr = GetComponent<SpriteRenderer>();
-        player = playerTrans.GetComponent<Player>();
+        mat = GetComponent<Renderer>().material;
     }
-    // Start is called before the first frame update
+
     void Start()
     {
-        bgSize = sr.sprite.rect.width * transform.localScale.x;
-        canvasSize = canvas.rect.width;
-        diff = (bgSize  - canvasSize) / sr.sprite.pixelsPerUnit;
-        mapLength = goal.position.x - respawnPoint.position.x;
-
-        playerSpeed = player.moveSpeed;
-        //transform.localPosition = new Vector3(moveLength / 2, 0, 10);
-        Debug.Log("canvas.size" + canvasSize);
-        Debug.Log("diff" + diff);
-        Debug.Log("====================");
-
+        offset = new Vector2(velocity, 0);
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
-        //Debug.Log(transform.localPosition);
-        //transform.position = new Vector3(moveLength / canvasSize - (moveLength * (player.transform.position.x - respawnPoint.position.x)) / mapLength , 0, 0);
-        //transform.localPosition = new Vector3(moveLength / 2 + (-moveLength * (player.transform.position.x - respawnPoint.position.x)) / mapLength, transform.localPosition.y, 10);
+        mat.mainTextureOffset += offset * Time.deltaTime;
     }
 }
